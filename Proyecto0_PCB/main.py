d2f48903d6036faba1b6b3c2359150a0ad1818c7
Marcefv <marcefv.89@gmail.com>
2020-08-31 20:32:50 -0600

    validacion de tags diferentes, tooltip con caracteristicas a genotipos

--- Proyecto0_PCB/main.py
+++ Proyecto0_PCB/main.py
@@ -115,66 +115,81 @@ class Main:
             else:
                 break
 
-        caract_genotipos = set()
+        caract_genotipos = {}
         list_genotipos =list()
         genotipos_combinacion = set()
-
+        inicial = ""
         #validaciones de los campos y generar lista de caracteristicas
         for index, widget in enumerate(self.lista_caracteristicas):
             # determinar cual informacion validadr dependiendo de si es visible o no
+
             if widget.get_visible():
+                if index == 0:
+                    inicial = widget.get_text()
                 # para las etiquetas o nombres de caracteristica
                 if "lab" in Gtk.Buildable.get_name(widget):
-                    caract_genotipos.add(widget.get_text().upper())
+                    caract_genotipos[widget.get_text()] = self.builder.get_object("txt_car_" + str(int(Gtk.Buildable.get_name(widget)[-1]))).get_text()
                     # determina si es solo texto y si no manda un mensaje de error y devuelve las etiquetas a los valores por defecto
                     # esto tambien valida si el campo esta vacio
                     if not widget.get_text().isalpha():
                         self.error("Solo puede colocar Caracteres alfabeticos\n\nNo puede dejar espacios vacios")
                         self.caracteristicas_por_defecto()
                         caract_genotipos.clear()
-                        break
+                        return
                     # valida si hay nombres repetidos, si es asi muestra mensaje de advertencia y vuelve a valores por defecto
-                    elif len(set(self.lista_caracteristicas)) != len(self.lista_caracteristicas):
-                        self.error("No puede haber nombres de caracteristicas repetidos")
-                        self.caracteristicas_por_defecto()
-                        caract_genotipos.clear()
-                        break
+                    elif not index == 0:
+                        if widget.get_text() == inicial:
+                            self.error("No puede colocar caracteres iguales")
+                            self.caracteristicas_por_defecto()
+                            caract_genotipos.clear()
+                            return
+                        else:
+                            inicial == widget.get_text()
 
                 # si es una descripcion, se valida si esta vacio, si esta vacio se muestra un mensaje de error
                 else:
                     if widget.get_text().strip() == "":
                         self.error("No puede dejar espacios vacios")
-                        caract_genotipos.clear()
-                        break
+                        return
 
-        for v in caract_genotipos:
-            print(v)
         #lista con combinaciones por caracteristica
-        for value in caract_genotipos:
+        set_caract_genotipos = {key.upper(): value for key, value in caract_genotipos.items()}
+        set_caract_genotipos = set(set_caract_genotipos.keys())
+        for value in set_caract_genotipos:
             list_genotipos.append(value + value)
             list_genotipos.append(value + value.lower())
             list_genotipos.append(value.lower() + value.lower())
 
         #lista de combinaciones entre caracteristicas
         genotipos_combinacion = list_genotipos
-        if len(caract_genotipos) == 2:
+        if len(set_caract_genotipos) == 2:
             genotipos_combinacion = list(map("".join, product(list_genotipos[0:3], list_genotipos[3:6])))
-        elif len(caract_genotipos) == 3:
+        elif len(set_caract_genotipos) == 3:
             genotipos_combinacion = list(map("".join, product(list_genotipos[0:3], list_genotipos[3:6], list_genotipos[6:9])))
-        elif len(caract_genotipos) == 4:
+        elif len(set_caract_genotipos) == 4:
             genotipos_combinacion = list(map("".join, product(list_genotipos[0:3], list_genotipos[3:6], list_genotipos[6:9], list_genotipos[9:13])))
 
         #agregar los genotipos a la pantalla de genotipos
         dict = {}
         for index, value in enumerate(genotipos_combinacion):
-             if index == 0:
+            caracteristica = ""
+            for key, val in caract_genotipos.items():
+                if key in genotipos_combinacion[index]:
+                    if key.isupper():
+                            caracteristica += "  " + val
+                    elif (key + key) in genotipos_combinacion[index]:
+                            caracteristica += "  " + val
+            #si es el primer elemento se crea un radio button y luego los demas van a pertenecer a ese grupo
+            if index == 0:
                  dict["rb{0}".format(index)] = Gtk.RadioButton(label=genotipos_combinacion[index])
                  dict["rb{0}".format(index)].connect("toggled", self.toggled_cb)
                  self.grid.attach_next_to(dict["rb{0}".format(index)], self.progenitor_1, Gtk.PositionType.BOTTOM, 1, 1)
                  dict["rb2{0}".format(index)] = Gtk.RadioButton(label=genotipos_combinacion[index])
                  dict["rb2{0}".format(index)].connect("toggled", self.toggled_cb)
                  self.grid.attach_next_to(dict["rb2{0}".format(index)], self.progenitor_2, Gtk.PositionType.BOTTOM, 1, 1)
-             else:
+                 dict["rb{0}".format(index)].set_tooltip_text(caracteristica)
+                 dict["rb2{0}".format(index)].set_tooltip_text(caracteristica)
+            else:
                  dict["rb{0}".format(index)] = Gtk.RadioButton.new_with_mnemonic_from_widget(
                      dict["rb0"], genotipos_combinacion[index])
                  dict["rb{0}".format(index)].connect("toggled", self.toggled_cb)
@@ -185,12 +200,19 @@ class Main:
                  dict["rb2{0}".format(index)].connect("toggled", self.toggled_cb)
                  self.grid.attach_next_to(dict["rb2{0}".format(index)], dict["rb2{0}".format(index - 1)],
                                           Gtk.PositionType.BOTTOM, 1, 1)
-             dict["rb{0}".format(index)].show()
-             dict["rb2{0}".format(index)].show()
+
+                 dict["rb{0}".format(index)].set_tooltip_text(caracteristica)
+                 dict["rb2{0}".format(index)].set_tooltip_text(caracteristica)
+            dict["rb{0}".format(index)].show()
+            dict["rb2{0}".format(index)].show()
+
+
 
     def toggled_cb(self, button):
         state = ""
         if button.get_active():
             state = "on"
+
+
 main = Main()
 Gtk.main()
